ac7acae60a7a617354b089f4f04439878addb656
Patrick Negus <negus.patrickyi@gmail.com>
2024-02-13 23:07:51 -0500

    fixed shrinking

--- personal-nextjs-site/src/app/layout.tsx
+++ personal-nextjs-site/src/app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👋</text></svg>"
+      />
         <PageTemplateLayout>
           {children}
         </PageTemplateLayout>
--- personal-nextjs-site/src/components/pageTemplateLayout.tsx
+++ personal-nextjs-site/src/components/pageTemplateLayout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Hey there!",
-  description: "Howdy! This is Patrick's Site built with Next.js.",
-};
+// export const metadata: Metadata = {
+//   title: "Hey there!",
+//   description: "Howdy! This is Patrick's Site built with Next.js.",
+// };
 
 export default function PageTemplateLayout({
   children,
@@ -15,9 +15,9 @@ export default function PageTemplateLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className = "grid grid-cols-3 w-full grid-cols-navbar">
+    <div className = "grid grid-cols-3 w-full grid-cols-navbar flex-shrink-0">
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between text-center p-36">
+      <main className="flex min-h-screen flex-col items-center justify-between text-center pt-36 pb-36">
           {children}
       </main>
     </div>
